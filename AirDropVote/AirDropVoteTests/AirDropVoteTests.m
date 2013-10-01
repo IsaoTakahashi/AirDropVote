3b70bc6380594f5999486a8464f9e91050bf36c0
IsaoTakahashi <ricto415@gmail.com>
2013-10-01 21:50:48 +0900

    Recreate project

---
+++ AirDropVote/AirDropVoteTests/AirDropVoteTests.m
@@ -0,0 +1,34 @@
+//
+//  AirDropVoteTests.m
+//  AirDropVoteTests
+//
+//  Created by 高橋 勲 on 2013/10/01.
+//  Copyright (c) 2013年 高橋 勲. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AirDropVoteTests : XCTestCase
+
+@end
+
+@implementation AirDropVoteTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
