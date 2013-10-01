0a8a8647df4f12492d744106d2a07190f67cfc08
IsaoTakahashi <ricto415@gmail.com>
2013-10-01 21:14:54 +0900

    Initial Commit

---
+++ AirDropVoting/AirDropVoting/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  AirDropVoting
+//
+//  Created by 高橋 勲 on 2013/10/01.
+//  Copyright (c) 2013年 高橋 勲. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ AirDropVoting/AirDropVoting/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AirDropVoting
+//
+//  Created by 高橋 勲 on 2013/10/01.
+//  Copyright (c) 2013年 高橋 勲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
